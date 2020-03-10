a635463d3069bedd9db9e8a276bc9b507df3354b
Rogansi <jtapper@tecnavia.com>
2020-03-10 17:02:47 -0500

    new images and some styling

--- App/components/ContactCard.js
+++ App/components/ContactCard.js
@@ -5,7 +5,11 @@ import PropTypes from 'prop-types'
 const ContactCard = ({person}) =>(
     <div className = "contactcard">
                    <p>{person.co_firstName+' '+person.co_lastName}</p>
-                   <p>{person.co_employer}</p>     
+                   <p>{person.co_title}</p>
+                   <p>{person.co_employer}</p>
+                   <div><a href = {"tel:"+person.co_phone}><img  className = "connectImage" src = {"/App/styles/images/contact_phone.png"}    /></a> 
+                   <a href = {"mailto:"+person.co_email}><img  className = "connectImage" src = {"/App/styles/images/contact_email.png"}    /></a>
+                   </div> 
     </div>
 )
 
